29ccd8c94ff0bc3139810ab2f03b4b4c556809c1
Steve Damon <stephen.m.damon@gmail.com>
2023-03-06 20:25:43 -0600

    cleanup

--- peepthisout.py
+++ peepthisout.py
@@ -1,6 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-srt = YouTubeTranscriptApi.get_transcript("9Om_a7Hr_8E")
-
-for line in srt:
-    print(line['text'].replace('\n',' '))
+if __name__ == '__main__':
+    srt = YouTubeTranscriptApi.get_transcript("9Om_a7Hr_8E")
+    for line in srt:    
+        print(line['text'].replace('\n',' '))
