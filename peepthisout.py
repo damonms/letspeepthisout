a934a324d3b7ae54024cbf7ef1519084accffbee
Steve Damon <stephen.m.damon@gmail.com>
2023-03-06 21:11:56 -0600

    scrape all dat situation

--- peepthisout.py
+++ peepthisout.py
@@ -4,18 +4,33 @@ import json
 import os
 API_KEY=os.environ['API_KEY_PEEP']
 CHANNEL_ID='UC8MbwfzshpM4T4miBkhqx5A'
+PAGE_TOKEN='CDIQAA'
 
+def get_next_page_token(response):
+    return response['nextPageToken']
+
+def get_next_page_response(page):
+    url_page = f'https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={CHANNEL_ID}&part=id&order=date&maxResults=50&pageToken={page}'
+    print(url_page)
+    return json.loads(requests.get(url_page).content)
 
 if __name__ == '__main__':
     # srt = YouTubeTranscriptApi.get_transcript("9Om_a7Hr_8E")
     # for line in srt:    
     #     print(line['text'].replace('\n',' '))
         
-    url = f'https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={CHANNEL_ID}&part=snippet,id&order=date&maxResults=2000'
-
-    r = requests.get(url)
+    url_base = f'https://www.googleapis.com/youtube/v3/search?key={API_KEY}&channelId={CHANNEL_ID}&part=id&order=date&maxResults=50'
+    
+    r = requests.get(url_base)
     
     # Print the results
+
     j = json.loads(r.content)
-    for item in j['items']:
-        print(item['id']['videoId'])
+    
+    while len(j['items']) ==50:
+        for item in j['items']:
+            print(item['id']['videoId'])
+            print(item)
+        next_token = get_next_page_token(j)
+        j = get_next_page_response(next_token)
+    
